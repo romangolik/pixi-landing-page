36fd6558abdc07ec8a2edd4da46455492c8e3aa8
Roman Golik <romangolik86786@gmail.com>
2022-07-29 10:19:37 +0300

    Created sidebar

--- src/index.js
+++ src/index.js
@@ -7,8 +7,34 @@ AOS.init({
     once: true
 });
 
+const SIDEBAR = document.getElementById('sidebar');
+const BURGER_BUTTONS = document.querySelectorAll('.burger');
+
+BURGER_BUTTONS.forEach(burger => {
+    burger.addEventListener('click', () => {
+        if (!burger.classList.contains('header__burger_active')) {
+            SIDEBAR.classList.add('sidebar_show');
+            BURGER_BUTTONS.forEach(item => item.classList.add('header__burger_active'));
+
+            const overlay = document.createElement('div');
+            overlay.classList.add('overlay');
+            document.querySelector('.header').appendChild(overlay);
+
+            overlay.addEventListener('click', () => {
+                BURGER_BUTTONS.forEach(item => item.classList.remove('header__burger_active'));
+                SIDEBAR.classList.remove('sidebar_show');
+                overlay.remove();
+            });
+        } else {
+            document.querySelector('.overlay').dispatchEvent(new Event('click'));
+        }
+    })
+});
+
+let activeLinkIndex = 0;
 const HEADER_LINKS = document.querySelectorAll('.header__navigation-item');
 const HEADER_NAVIGATION_MARKER = document.querySelector('.header__navigation-marker');
+const SIDEBAR_LINKS = document.querySelectorAll('.sidebar__navigation-item');
 
 const setActiveLink = selectedIndex => {
     HEADER_LINKS.forEach((headerLink, index) => {
@@ -16,6 +42,7 @@ const setActiveLink = selectedIndex => {
             headerLink.classList.remove('link_active');
         } else {
             headerLink.classList.add('link_active');
+            moveNavigationMarker(headerLink);
         }
     });
 }
@@ -28,11 +55,29 @@ HEADER_LINKS.forEach((headerLink, linkIndex) => {
     headerLink.addEventListener('click', event => {
         event.preventDefault();
         setActiveLink(linkIndex);
-        moveNavigationMarker(headerLink);
+        activeLinkIndex = linkIndex;
     });
 });
 
-moveNavigationMarker(HEADER_LINKS[0]);
+SIDEBAR_LINKS.forEach((sidebarLink, linkIndex) => {
+    sidebarLink.addEventListener('click', event => {
+        event.preventDefault();
+        activeLinkIndex = linkIndex;
+    });
+});
+
+setActiveLink(activeLinkIndex);
+
+window.addEventListener('resize', () => {
+    const overlay = document.querySelector('.overlay');
+    if (window.innerWidth >= 1023) {
+        setActiveLink(activeLinkIndex);
+
+        if (overlay) {
+            overlay.dispatchEvent(new Event('click'));
+        }
+    }
+});
 
 
 
